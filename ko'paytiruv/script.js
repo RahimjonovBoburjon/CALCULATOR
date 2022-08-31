72af594a6b9b68d96fb7d21781e0c7251620a1cb
{Boburjon} <111169106+RahimjonovBoburjon@users.noreply.github.com>
2022-08-31 11:55:40 +0500

    CALCULATOR

---
+++ ko'paytiruv/script.js
@@ -0,0 +1,13 @@
+// SETUP-1
+const text1 = document.querySelector(".text1");
+const text2 = document.querySelector(".text2");
+const three = document.querySelector(".EQUAL");
+const btn = document.querySelector(".btn");
+// SETUP-2
+text1.addEventListener("input", () => console.log(text1.value));
+text2.addEventListener("input", () => console.log(text2.value));
+btn.addEventListener("click", myCalc);
+// SETUP-3
+function myCalc() {
+    three.value = (+text1.value * +text2.value);
+};
